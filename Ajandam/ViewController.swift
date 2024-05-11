78599a5a6db1ad6a637952c09bc2a034242f468e
yigitbostanci <yigit.bstnci@gmail.com>
2024-05-12 02:51:16 +0300

    Initial Commit

---
+++ Ajandam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ajandam
+//
+//  Created by Yigit Bostanci on 12.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
