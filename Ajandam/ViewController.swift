41dcd263e449f06eafb8fc79a639126df2c9af9b
yigitbstnci <yigit.bstnci@gmail.com>
2024-05-12 03:10:22 +0300

    initial Commit

--- Ajandam/ViewController.swift
+++ Ajandam/ViewController.swift
@@ -13,7 +13,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-//first Commits
-
+    //innitial Push
 }
 
