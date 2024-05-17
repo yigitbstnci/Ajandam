60d7d58b02064fd0610c23731cadc8b0bd1709fa
yigitbstnci <yigit.bstnci@gmail.com>
2024-05-18 01:20:03 +0300

    .

--- Ajandam/View/TasksCollectionViewController.swift
+++ Ajandam/View/TasksCollectionViewController.swift
@@ -14,6 +14,9 @@ var selectedCellIds: [String] = []
 
 class TasksCollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     
+        let floatButton = UIButton(type: .custom)
+        
+    
     // MARK: - Fetch Data
     
     func fetchData() {
@@ -29,60 +32,15 @@ class TasksCollectionViewController: UICollectionViewController, UICollectionVie
         collectionView.allowsSelection = false
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonTapped))
         navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: #selector(editButtonTapped))
+        
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         fetchData()
     }
-    
-    // MARK: - Button Actions
-    
-    @objc func addButtonTapped() {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let detailVC = storyboard.instantiateViewController(withIdentifier: "DetailViewController") as! TaskDetailViewController
-        navigationController?.pushViewController(detailVC, animated: true)
-    }
-    
-    @objc func editButtonTapped() {
-        collectionView.allowsMultipleSelection = true
-        collectionView.allowsSelection = true
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .trash, target: self, action: #selector(deleteButtonTapped))
-        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done , target: self, action: #selector(closeEditMode))
-    }
-    
-    @objc func deleteButtonTapped() {
-        collectionView.allowsMultipleSelection = false
-        collectionView.allowsSelection = false
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonTapped))
-        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: #selector(editButtonTapped))
         
-        // Deselect the selected item and set the background color to white
-        for indexPath in collectionView.indexPathsForVisibleItems {
-            collectionView.deselectItem(at: indexPath, animated: true)
-            let cell = collectionView.cellForItem(at: indexPath)
-            cell?.backgroundColor = .white
-        }
-        
-        // Remove the selected tasks
-        for id in selectedCellIds {
-            if let index = tasks.firstIndex(where: { $0.id == id }) {
-                TaskManager.shared.deleteTask(at: index)
-            }
-        }
-        
-        // Reload collection view data
-        fetchData()
-    }
-    
-    @objc func closeEditMode() {
-        collectionView.allowsMultipleSelection = false
-        collectionView.allowsSelection = false
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonTapped))
-        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: #selector(editButtonTapped))
-        collectionView.visibleCells.forEach {$0.backgroundColor = .white}
-    }
-    
     // MARK: - UICollectionViewDataSource
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -101,6 +59,13 @@ class TasksCollectionViewController: UICollectionViewController, UICollectionVie
         cell?.backgroundColor = .lightGray
         let selectedId = tasks[indexPath.item].id
         selectedCellIds.append(selectedId)
+        if selectedCellIds.count == 1 {
+            floatButton.isHidden = false
+            print("burda kaldı 1 ")
+        }else {
+            floatButton.isHidden = true
+            print("burda kaldı 2")
+        }
     }
     
     override func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
@@ -109,13 +74,140 @@ class TasksCollectionViewController: UICollectionViewController, UICollectionVie
         let deselectedId = tasks[indexPath.item].id
         if let index = selectedCellIds.firstIndex(of: deselectedId) {
             selectedCellIds.remove(at: index)
+            if selectedCellIds.isEmpty {
+                floatButton.isHidden = true
+                print("burda kaldı 3 ")
+            }else if selectedCellIds.count > 1 {
+                floatButton.isHidden = true
+                print("burda kaldı 4 ")
+            }else   {
+                floatButton.isHidden = false
+                print("burda kaldı 5 ")
+                print("\(selectedCellIds)")
+            }
         }
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: view.frame.width, height: 100)
     }
+    
+    // MARK: - Button Actions, activity
+    
+    @objc func addButtonTapped() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let detailVC = storyboard.instantiateViewController(withIdentifier: "DetailViewController") as! TaskDetailViewController
+        navigationController?.pushViewController(detailVC, animated: true)
+    }
+    
+    @objc func editButtonTapped() {
+        collectionView.allowsMultipleSelection = true
+        collectionView.allowsSelection = true
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .trash, target: self, action: #selector(deleteButtonTapped))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done , target: self, action: #selector(closeEditMode))
+        createFloatButton()
+    }
+    
+    
+    @objc func deleteButtonTapped() {
+        collectionView.allowsMultipleSelection = false
+        collectionView.allowsSelection = false
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonTapped))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: #selector(editButtonTapped))
+        //alert for remove Task
+        let ac = UIAlertController(title: "Ajandam", message: "Planlanmış Görevi Silmek İster misin ?", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Sil", style: .default, handler: deleteFunc))
+        ac.addAction(UIAlertAction(title: "Vazgeç", style: .destructive, handler: nil))
+        present(ac, animated: true)
+        
+        // Deselect the selected item and set the background color to white
+        for indexPath in collectionView.indexPathsForVisibleItems {
+            collectionView.deselectItem(at: indexPath, animated: true)
+            let cell = collectionView.cellForItem(at: indexPath)
+            cell?.backgroundColor = .white
+        }
+        
+    }
+    
+    @objc func closeEditMode() {
+        collectionView.allowsMultipleSelection = false
+        collectionView.allowsSelection = false
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonTapped))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: #selector(editButtonTapped))
+        collectionView.visibleCells.forEach {$0.backgroundColor = .white}
+        floatButton.isHidden = true
+    }
+    
+    func deleteFunc(alert: UIAlertAction) {
+        // Remove the selected tasks
+        for id in selectedCellIds {
+            if let index = tasks.firstIndex(where: { $0.id == id }) {
+                TaskManager.shared.deleteTask(at: index)
+            }
+        }
+        // Reload collection view data
+        fetchData()
+        floatButton.isHidden = true
+    }
+
+    
+    func createFloatButton () {
+        floatButton.setImage(UIImage(named: "pencil"), for: .normal)
+        floatButton.backgroundColor = .systemBlue
+        floatButton.setTitleColor(.white, for: .normal)
+        floatButton.layer.cornerRadius = 25
+        floatButton.layer.masksToBounds = true
+        floatButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(floatButton)
+        floatButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20).isActive = true
+        floatButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+        floatButton.widthAnchor.constraint(equalToConstant: 50).isActive = true
+        floatButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        floatButton.addTarget(self, action: #selector(floatButtonTapped), for: .touchUpInside)
+        floatButton.isHidden = true
+    }
+    
+    @objc func floatButtonTapped() {
+        //selected task ID navigate to detailpage to show
+        if let selectedTaskID = selectedCellIds.first {
+        }
+       
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // MARK: - TaskCell
 
@@ -134,5 +226,7 @@ class TaskCell: UICollectionViewCell {
         dateLabel.text = dateString
         taskTypeImage.image = UIImage(named: task.image)
         descriptionLabel.sizeToFit()
+        descriptionLabel.numberOfLines = 2
+        
     }
 }
